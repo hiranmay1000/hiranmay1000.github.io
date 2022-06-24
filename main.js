79a109a715e1d788ef4e94b804c76f44e69a52b5
Seeker <hiranmay1000@gmail.com>
2022-06-24 19:57:57 +0530

    version 2.6150

--- main.js
+++ main.js
@@ -19,27 +19,48 @@ querySelector.addEventListener(".nav-link").forEach(element => {
 
 // ==============NAV-BAR-ONSCROLL===============
 function scrollFunction() {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30 ) {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
         document.getElementById("navbar").style.position = "fixed";
         document.getElementById("navbar").style.zIndex = "4";
-        document.getElementById("navbar").style.top = "0%";
+        document.getElementById("navbar").style.top = "0px";
         document.getElementById("navbar").style.height = "60px";
         document.getElementById("navbar").style.display = "flex";
         document.getElementById("navbar").style.justifyContent = "space-between";
-
+        
+        
         document.getElementById("login-container").style.display = "flex";
-        document.getElementById("logo").style.fontSize = "25px";
+        document.getElementById("login-container").style.alignItems = "center";
+        document.getElementById("login-container").style.justifyContent = "center";
+        document.getElementById("login-container").style.height = "60px";
+        document.getElementById("login-btn").style.width = "80px";
+        document.getElementById("login-btn").style.height = "35px";
+
+        document.getElementById("nav-menu").style.height = "60px";
+
+        document.getElementById("logo").style.fontSize = "28px";
         document.getElementById("logo").style.lineHeight = "60px";
+
+        document.getElementById("index-banner").style.paddingTop = "80px";
+
         
-        document.getElementsByClassName("nav-list-items").classList.display = ""
-        
+        ["link1", "link2", "link3", "link4", "link5"].forEach(function (id) {
+            document.getElementById(id).style.lineHeight = "60px";
+            document.getElementById(id).style.fontSize = "14px";
+        });
         
     } else {
+
         document.getElementById("nav-menu").style.float = "left";
         document.getElementById("navbar").style.height = "80px";
-        document.getElementById("navbar").style.top = "3%"; 
+        document.getElementById("navbar").style.top = "25px";
         document.getElementById("navbar").style.display = "flex";
+        document.getElementById("navbar").style.alignItems = "center";
         document.getElementById("navbar").style.justifyContent = "space-between";
+
+        document.getElementById("login-container").style.display = "flex";
+        document.getElementById("login-container").style.width = "280px";
+        document.getElementById("login-btn").style.width = "85px";
+        document.getElementById("login-btn").style.height = "38px";
 
         document.getElementById("nav-menu").style.height = "80px";
         document.getElementById("nav-menu").style.float = "left";
@@ -50,6 +71,13 @@ function scrollFunction() {
         document.getElementById("logo").style.display = "block";
         document.getElementById("logo").style.fontSize = "35px";
         document.getElementById("logo").style.lineHeight = "80px";
+
+        ["link1", "link2", "link3", "link4", "link5"].forEach(function (id) {
+            document.getElementById(id).style.display = "block";
+            document.getElementById(id).style.lineHeight = "80px";
+            document.getElementById(id).style.fontSize = "16px";
+        });
+
     }
 }
 // ==============NAV-BAR-ONSCROLL-END===============
