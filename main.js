4a153b502703a38606efbbc070ec8b138971f130
Hiranmay <hiranmay1000@gmail.com>
2022-07-05 16:32:31 +0530

    2.8100

--- main.js
+++ main.js
@@ -58,20 +58,12 @@ function scrollFunction() {
         document.getElementById("navbar").style.zIndex = "4";
         document.getElementById("navbar").style.top = "0%";
         document.getElementById("navbar").style.height = "45px";
-        // document.getElementById("navbar").style.display = "flex";
-        // document.getElementById("navbar").style.justifyContent = "space-between";
 
-
-        // document.getElementById("login-container").style.display = "flex";
-        // document.getElementById("login-container").style.alignItems = "center";
-        // document.getElementById("login-container").style.justifyContent = "center";
         document.getElementById("login-container").style.height = "45px";
 
         document.getElementById("login-btn").style.width = "75px";
         document.getElementById("login-btn").style.height = "25px";
         document.getElementById("login-btn").style.fontSize = "8px";
-
-        document.getElementById("nav-menu").style.marginTop = "23px";
 
         document.getElementById("logo").style.fontSize = "22px";
         document.getElementById("logo").style.lineHeight = "45px";
@@ -92,22 +84,13 @@ function scrollFunction() {
 
         document.getElementById("navbar").style.height = "80px";
         document.getElementById("navbar").style.top = "25px";
-        // document.getElementById("navbar").style.display = "flex";
         document.getElementById("navbar").style.alignItems = "center";
-        // document.getElementById("navbar").style.justifyContent = "space-between";
 
-        // document.getElementById("login-container").style.display = "flex";
-        // document.getElementById("login-container").style.width = "280px";
         document.getElementById("login-btn").style.width = "100px";
         document.getElementById("login-btn").style.height = "38px";
         document.getElementById("login-btn").style.fontSize = "12px";
 
-        // document.getElementById("nav-menu").style.height = "80px";
-        // document.getElementById("nav-menu").style.justifyContent = "center";
-        // document.getElementById("nav-menu").style.alignItems = "center";
-        // document.getElementById("nav-menu").style.flexDirection = "coloumn";
-        document.getElementById("nav-menu").style.marginTop = "40px";
-
+        document.getElementById("hamburger").style.padding = "12px";
 
         document.getElementById("logo").style.fontSize = "32px";
         document.getElementById("logo").style.lineHeight = "80px";
