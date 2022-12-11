76065776c7e36ae58c76b96573e1994f31c61b1b
Max <hiranmay1000@gmail.com>
2022-12-12 00:38:40 +0530

    4.1

--- utilities.js
+++ utilities.js
@@ -4,31 +4,7 @@ btn.addEventListener('click', updateSubs);
 function updateSubs() {
   if (btn.textContent.includes('SUBSCRIBE')) {
     btn.textContent = "SUBSCRIBED";
-  }else if (btn.textContent.includes('SUBSCRIBED')){
+  } else if (btn.textContent.includes('SUBSCRIBED')) {
     btn.textContent = "SUBSCRIBE";
-  }
-}
-
-// ........................==HAMBURGER-ICON.....................
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("#nav-menu");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}));
-// .....................HAMBURGER-ICON-END.....................
-
-function myFunction() {
-  var x = document.getElementById("nav-menu");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
   }
 }
